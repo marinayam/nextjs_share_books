b851bfe1feb1121ec29847089e9efce2d05465c3
marina <91815032+marinayam@users.noreply.github.com>
2024-08-04 12:52:27 +0900

    ✨ homeページ

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        ReadTogether
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          A Community for Book Lovers
+        </span>
+      </h1>
+      <p className="desc text-center">
+        読書を通じて人々をつなげるアプリです。
+        <br />
+        読んだ本を簡単に共有し、新しい発見を得ることができます。
+      </p>
+    </section>
+  );
+};
+
+export default Home;
